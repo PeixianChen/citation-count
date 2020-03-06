c325311eca3d1cb7073507411b930839a343fa43
Peixian Chen <pealing@foxmail.com>
2020-03-07 00:51:25 +0800

    Update core.py

--- core.py
+++ core.py
@@ -2,6 +2,9 @@ from typing import List
 
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 from utils import wait_captcha, Article
 
@@ -61,7 +64,31 @@ class GoogleScholar(webdriver.Chrome):
             except NoSuchElementException:
                 authors = 'Not Available'
 
-            articles.append(Article(title, url, authors))
+            # 引用源
+            # 打开该文章引用源
+            # WebDriverWait(self,30).until(EC.presence_of_element_located((By.XPATH,'./div/a[@class="gs_or_cit gs_nph"]')))
+            btn_citation = div_article.find_element_by_xpath('./div/a[@class="gs_or_cit gs_nph"]')    
+            btn_citation.click()
+            import time; time.sleep(2)  # 这里要等引用的表格出来才能继续
+            # 检索引用源
+            WebDriverWait(self,30).until(EC.presence_of_element_located((By.XPATH,'//div[@id="gs_citt"]')))
+            tab_sources = self.find_element_by_xpath('//div[@id="gs_citt"]')
+            sources = tab_sources.text.split('\n')
+
+            # 在这里要把引用的表格关掉才能继续
+            WebDriverWait(self,30).until(EC.presence_of_element_located((By.XPATH,'//a[@id="gs_cit-x"]')))
+            btn_close_citation = self.find_element_by_xpath('//a[@id="gs_cit-x"]')
+            btn_close_citation.click()
+
+            # import time; time.sleep(4)  
+
+            # 处理引用源
+            citation = dict()
+            for i in range(0, len(sources), 2):
+                citation[sources[i]] = sources[i + 1]
+            # print(citation['APA'])
+
+            articles.append(Article(title, url, citation['APA']))
 
         return articles
 
